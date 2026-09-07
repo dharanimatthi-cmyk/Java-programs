d54ccb9fb34adf7ab933d1a940c6bb65103fd641
dharanimatthi-cmyk <dharanimatthi@gmail.com>
2026-09-07 10:35:32 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,27 @@
+//*package CIET;
+ //class Animal {
+	//void eat() {
+		//System.out.println("Animal eat");
+	//}
+//}
+ //class Dog extends Animal {
+	// void bark() {
+		 //System.out.println("Barking");
+	// }
+ //}
+ //class Cat extends Animal {
+	 //void moew() {
+		 //System.out.println("Moewing");
+	// }
+ //}
+ //class Animals {
+	 //public static void main(String[] args) {
+		// Dog d=new Dog();
+		 //Cat c=new Cat();
+		 //d.bark();
+		 //d.eat();
+		 //c.moew();
+		 //c.eat();
+	 //}
+// }
+*/
