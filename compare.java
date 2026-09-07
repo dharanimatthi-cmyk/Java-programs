c6cf60a2741fef226db4f5bc4f660ef2a8bccdf4
dharanimatthi-cmyk <dharanimatthi@gmail.com>
2026-09-07 10:45:09 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,31 @@
+/*package CIET;
+import java.util.*;
+
+class Student implements Comparable<Student>{
+	String name;
+	Student(String name){
+		this.name=name;
+	}
+	public int compareTo(Student other) {
+		return this.name.compareTo(other.name);
+	}
+	public String toString() {
+		return name;
+	}
+}
+public class compare {
+	public static void main(String[] args) {
+		List<Student> s=new ArrayList<>();
+		s.add(new Student("rahul"));
+		s.add(new Student("mahi"));
+		s.add(new Student("rani"));
+		s.add(new Student("layi"));
+		System.out.println("----Before Sorting----");
+		System.out.println(s);
+		Collections.sort(s);
+		System.out.println("----After Sorting----");
+		System.out.println(s);
+	}
+
+}
+*/
