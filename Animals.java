e3e5c9c8ec38ee5997927ae07f8e047932432c37
dharanimatthi-cmyk <dharanimatthi@gmail.com>
2026-09-07 10:38:18 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,37 @@
+//*package CIET;
+//import java.util.Scanner;
+//
+//class Animals {
+//	void eat() {
+//		System.out.println("Animal is eating");
+//	}
+//
+//}
+//class Dog extends Animal {
+//	void bark() {
+//		System.out.println("Dog barks");
+//	}
+//	
+//}
+//class Cat extends Dog {
+//	void meow() {
+//		System.out.println("cat meow");
+//	}
+//}
+//class Rat extends Cat {
+//	void hide() {
+//		System.out.println("rat hide");
+//	}
+//}
+//
+//
+//
+//public class Animals {
+//	public static void main(String[] args) {
+//		Rat r=new Rat();
+//		r.eat();
+//		r.bark();
+//		r.meow();
+//		r.hide();
+//	}
+*//}
